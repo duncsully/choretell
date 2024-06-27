0f96d3931c2081d9724830800fbbc16cb718be66
duncsully <duncsully@gmail.com>
2024-06-27 16:53:48 -0700

    refactor: break up component
    add rudimentary error handling
    
    rename item -> chore
    
    improve add sheet dismissal UX

--- src/pages/ChoreList/ChoreList.ts
+++ src/pages/ChoreList/ChoreList.ts
@@ -1,5 +1,13 @@
 import { ChoresResponse } from './../../../pocketbase-types'
-import { component, computedGroup, effect, html, signal } from 'solit'
+import {
+  Computed,
+  Signal,
+  component,
+  computedGroup,
+  effect,
+  html,
+  signal,
+} from 'solit'
 import { pb } from '../../globals'
 import { ref, createRef } from 'lit-html/directives/ref.js'
 import { repeat } from 'lit-html/directives/repeat.js'
@@ -7,16 +15,15 @@ import { repeat } from 'lit-html/directives/repeat.js'
 import '@ionic/core/components/ion-textarea'
 import { showConfirm } from '../../components/Confirmation'
 
-// TODO: Separate out components
+// TODO: Turn window alerts into toasts?
 // TODO: Utility for working with forms
-// TODO: Standardize on modal handling (dismiss + canDismiss vs manual close?)
 export const ChoreListPage = component(() => {
-  const items = signal([] as ChoresResponse[])
+  const chores = signal([] as ChoresResponse[])
 
   effect(() => {
     const getItems = () => {
       console.log('fetching chores')
-      pb.collection('chores').getFullList().then(items.set)
+      pb.collection('chores').getFullList().then(chores.set)
     }
     getItems()
     // TODO: Not efficient, but works for now
@@ -25,7 +32,7 @@ export const ChoreListPage = component(() => {
   })
 
   const [notDone, done] = computedGroup(() =>
-    items.get().reduce<[ChoresResponse[], ChoresResponse[]]>(
+    chores.get().reduce<[ChoresResponse[], ChoresResponse[]]>(
       (results, item) => {
         results[+item.done].push(item)
         return results
@@ -36,63 +43,14 @@ export const ChoreListPage = component(() => {
 
   const isAdding = signal(false)
 
-  // TODO: Refactor to just regenerate the form?
-  const addFormRef = createRef<HTMLFormElement>()
-  const resetForm = () => {
-    isAdding.set(false)
-    const form = addFormRef.value
-    form?.reset()
-    // Text areas don't reset, need to manually clear them
-    form?.querySelectorAll('ion-textarea').forEach((el) => (el.value = ''))
-  }
+  const editingChore = signal(null as ChoresResponse | null)
 
-  const handleAdd = (e: SubmitEvent) => {
-    e.preventDefault()
-    const form = new FormData(e.target as HTMLFormElement)
-    pb.collection('chores').create(form)
-    resetForm()
-  }
-
-  // Added as a prop, so need to wrap in a signal
-  const canDismissAdd = signal(() => {
-    const formData = new FormData(addFormRef.value!)
-    if ([...formData.values()].some(Boolean)) {
-      return showConfirm({
-        header: 'Discard draft?',
-        message: 'Are you sure you want to discard your draft?',
-        confirmText: 'Discard',
-      })
-    }
-    return true
-  })
-
-  const editingItem = signal(null as ChoresResponse | null)
-  const handleEdit = (e: SubmitEvent) => {
-    e.preventDefault()
-    const form = new FormData(e.target as HTMLFormElement)
-    pb.collection('chores').update(editingItem.get()?.id!, form)
-    editingItem.reset()
-  }
-  const handleEditBack = async () => {
-    const formData = new FormData(editFormRef.get() as HTMLFormElement)
-    let discard = true
-    if (
-      [...formData.entries()].some(
-        ([key, value]) =>
-          value !== editingItem.get()?.[key as keyof ChoresResponse]
-      )
-    ) {
-      discard = await showConfirm({
-        header: 'Discard changes?',
-        message: 'Are you sure you want to discard your changes?',
-        confirmText: 'Discard',
-      })
-    }
-    if (discard) {
-      editingItem.reset()
-    }
-  }
-  const editFormRef = signal<Element | undefined>(undefined)
+  const makeChoreList = (list: Computed<ChoresResponse[]>) => () =>
+    repeat(
+      list.get(),
+      (chore) => `${chore.id}_${chore.updated}`,
+      (chore) => ChoreItem(chore, () => editingChore.set(chore))
+    )
 
   return html`
     <ion-header>
@@ -107,27 +65,13 @@ export const ChoreListPage = component(() => {
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding" fixed-slot-placement="before">
-      <ion-list>
-        ${() =>
-          repeat(
-            notDone.get(),
-            (item) => `${item.id}_${item.updated}`,
-            (item) => Item(item, () => editingItem.set(item))
-          )}
-      </ion-list>
+      <ion-list> ${makeChoreList(notDone)} </ion-list>
       <ion-accordion-group>
         <ion-accordion>
           <ion-item slot="header">
             <ion-label>Completed (${() => done.get().length})</ion-label>
           </ion-item>
-          <ion-list slot="content">
-            ${() =>
-              repeat(
-                done.get(),
-                (item) => `${item.id}_${item.updated}`,
-                (item) => Item(item, () => editingItem.set(item))
-              )}
-          </ion-list>
+          <ion-list slot="content"> ${makeChoreList(done)} </ion-list>
         </ion-accordion>
       </ion-accordion-group>
       <ion-fab slot="fixed" vertical="bottom" horizontal="end">
@@ -135,34 +79,140 @@ export const ChoreListPage = component(() => {
           <ion-icon name="add"></ion-icon>
         </ion-fab-button>
       </ion-fab>
-      <ion-modal
-        .isOpen=${isAdding}
-        .canDismiss=${canDismissAdd}
-        @didDismiss=${resetForm}
-        .breakpoints=${[0, 1]}
-        initial-breakpoint="1"
-        backdrop-breakpoint="0.5"
-        style="--height: auto;"
-      >
-        <div class="ion-padding">
-          <form
-            ${ref(addFormRef)}
-            @submit=${handleAdd}
-            style="display: flex; flex-direction: column;"
-          >
-            <ion-input placeholder="Chore name" name="name"></ion-input>
-            <ion-textarea
-              placeholder="Description"
-              auto-grow
-              name="description"
-            ></ion-textarea>
-            <ion-button type="submit" fill="clear" style="align-self: end;">
-              Add
-            </ion-button>
-          </form>
-        </div>
-      </ion-modal>
-      <ion-modal .isOpen=${() => !!editingItem.get()}>
+    </ion-content>
+    ${ChoreAddForm(isAdding)} ${ChoreEditModal(editingChore)}
+  `
+})
+
+const ChoreItem = component((chore: ChoresResponse, onClick: () => void) => {
+  const handleCheck = (e: Event) => {
+    e.stopPropagation()
+    pb.collection('chores').update(chore.id, { done: !chore.done })
+  }
+  return html`
+    <ion-item button @click=${onClick}>
+      <ion-button slot="start" fill="clear" size="large" @click=${handleCheck}>
+        <ion-icon
+          name=${chore.done ? 'checkbox-outline' : 'square-outline'}
+          slot="icon-only"
+        ></ion-icon>
+      </ion-button>
+      <ion-label class="ion-text-nowrap">
+        <span style=${chore.done ? 'text-decoration: line-through;' : ''}>
+          ${chore.name}
+        </span>
+        <p>${chore.description}</p>
+      </ion-label>
+    </ion-item>
+  `
+})
+
+const ChoreAddForm = component((isAdding: Signal<boolean>) => {
+  const addFormRef = createRef<HTMLFormElement>()
+  const resetForm = () => {
+    const form = addFormRef.value
+    form?.reset()
+    // Text areas don't reset, need to manually clear them
+    form?.querySelectorAll('ion-textarea').forEach((el) => (el.value = ''))
+  }
+
+  const handleAdd = async (e: SubmitEvent) => {
+    e.preventDefault()
+    try {
+      const form = new FormData(e.target as HTMLFormElement)
+      await pb.collection('chores').create(form)
+      isAdding.set(false)
+      resetForm()
+    } catch (err) {
+      console.error(err)
+      window.alert('Failed to add item')
+    }
+  }
+
+  const handleDismiss = async () => {
+    const formData = new FormData(addFormRef.value!)
+    isAdding.set(false)
+    let discard = true
+    if ([...formData.values()].some(Boolean)) {
+      discard = await showConfirm({
+        header: 'Discard draft?',
+        message: 'Are you sure you want to discard your draft?',
+        confirmText: 'Discard',
+      })
+    }
+    if (discard) {
+      resetForm()
+    } else {
+      isAdding.set(true)
+    }
+  }
+
+  return html`
+    <ion-modal
+      .isOpen=${isAdding}
+      @didDismiss=${handleDismiss}
+      .breakpoints=${[0, 1]}
+      initial-breakpoint="1"
+      backdrop-breakpoint="0.5"
+      style="--height: auto;"
+    >
+      <div class="ion-padding">
+        <form
+          ${ref(addFormRef)}
+          @submit=${handleAdd}
+          style="display: flex; flex-direction: column;"
+        >
+          <ion-input placeholder="Chore name" name="name"></ion-input>
+          <ion-textarea
+            placeholder="Description"
+            auto-grow
+            name="description"
+          ></ion-textarea>
+          <ion-button type="submit" fill="clear" style="align-self: end;">
+            Add
+          </ion-button>
+        </form>
+      </div>
+    </ion-modal>
+  `
+})
+
+const ChoreEditModal = component(
+  (editingChore: Signal<ChoresResponse | null>) => {
+    const handleEdit = async (e: SubmitEvent) => {
+      e.preventDefault()
+      try {
+        const form = new FormData(e.target as HTMLFormElement)
+        await pb.collection('chores').update(editingChore.get()?.id!, form)
+        editingChore.reset()
+      } catch (err) {
+        console.error(err)
+        window.alert('Failed to update item')
+      }
+    }
+    const handleEditBack = async () => {
+      const formData = new FormData(editFormRef.get() as HTMLFormElement)
+      let discard = true
+      if (
+        [...formData.entries()].some(
+          ([key, value]) =>
+            value !== editingChore.get()?.[key as keyof ChoresResponse]
+        )
+      ) {
+        discard = await showConfirm({
+          header: 'Discard changes?',
+          message: 'Are you sure you want to discard your changes?',
+          confirmText: 'Discard',
+        })
+      }
+      if (discard) {
+        editingChore.reset()
+      }
+    }
+    const editFormRef = signal<Element | undefined>(undefined)
+
+    return html`
+      <ion-modal .isOpen=${() => !!editingChore.get()}>
         <ion-header>
           <ion-toolbar>
             <ion-buttons slot="start">
@@ -177,14 +227,14 @@ export const ChoreListPage = component(() => {
             <ion-input
               placeholder="Chore name"
               name="name"
-              .value=${() => editingItem.get()?.name}
+              .value=${() => editingChore.get()?.name}
               style="font-size: 1.5em;"
             ></ion-input>
             <ion-textarea
               placeholder="Add details"
               auto-grow
               name="description"
-              .value=${() => editingItem.get()?.description}
+              .value=${() => editingChore.get()?.description}
             >
               <ion-icon name="reader-outline" slot="start"></ion-icon>
             </ion-textarea>
@@ -205,29 +255,6 @@ export const ChoreListPage = component(() => {
           </ion-toolbar>
         </ion-footer>
       </ion-modal>
-    </ion-content>
-  `
-})
-
-const Item = component((item: ChoresResponse, onClick: () => void) => {
-  const handleCheck = (e: Event) => {
-    e.stopPropagation()
-    pb.collection('chores').update(item.id, { done: !item.done })
+    `
   }
-  return html`
-    <ion-item button @click=${onClick}>
-      <ion-button slot="start" fill="clear" size="large" @click=${handleCheck}>
-        <ion-icon
-          name=${item.done ? 'checkbox-outline' : 'square-outline'}
-          slot="icon-only"
-        ></ion-icon>
-      </ion-button>
-      <ion-label class="ion-text-nowrap">
-        <span style=${item.done ? 'text-decoration: line-through;' : ''}>
-          ${item.name}
-        </span>
-        <p>${item.description}</p>
-      </ion-label>
-    </ion-item>
-  `
-})
+)
