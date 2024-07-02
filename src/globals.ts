1a25016fb6c6421e259066f0bdb1f186d8ce89ed
duncsully <duncsully@gmail.com>
2024-07-01 18:35:01 -0700

    fix: attempt setting prod pb URL from env

--- src/globals.ts
+++ src/globals.ts
@@ -2,4 +2,8 @@ import PocketBase from 'pocketbase'
 import { TypedPocketBase } from '../pocketbase-types'
 
 // TODO: Use environment variables
-export const pb = new PocketBase('http://localhost:8090') as TypedPocketBase
+export const pb = new PocketBase(
+  import.meta.env.DEV
+    ? 'http://localhost:8090'
+    : import.meta.env.VITE_POCKETBASE_URL
+) as TypedPocketBase
