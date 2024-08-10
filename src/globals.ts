d344bd7702d976b64ef20bd0efe068c2124a0cc4
duncsully <duncsully@gmail.com>
2024-08-09 17:31:55 -0700

    fix: assume that PB API is from same origin as web server (which is currently PB)

--- src/globals.ts
+++ src/globals.ts
@@ -1,9 +1,6 @@
 import PocketBase from 'pocketbase'
 import { TypedPocketBase } from '../pocketbase-types'
 
-// TODO: Use environment variables
 export const pb = new PocketBase(
-  import.meta.env.DEV
-    ? 'http://localhost:8090'
-    : import.meta.env.VITE_POCKETBASE_URL
+  import.meta.env.DEV ? 'http://localhost:8090' : window.location.origin
 ) as TypedPocketBase
